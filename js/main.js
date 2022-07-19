035ca467d03fbca32c41ac923032e28f341b1ea8
SemFlinn <100510929+SemFlinn@users.noreply.github.com>
2022-07-19 13:51:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -136,14 +136,16 @@ if (iconMenu){
 }
 //=================================================================================================================================================
 // Класс для перевода url картинки в backgraund картинки
-setTimeout(function ibg(){
-  let ibg = document.querySelectorAll(".ibg");
+function ibg() {
+        let ibg = document.querySelectorAll(".ibg");
         for (var i = 0; i < ibg.length; i++) {
         if(ibg[i].querySelector('img')) {
             ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
         }
     }
-}, 5000);
+}
+
+ibg();
 
 //===============================================================================================================================
 
