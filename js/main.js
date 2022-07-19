eada13d91708a65483434dd3d30edd439c17075a
SemFlinn <100510929+SemFlinn@users.noreply.github.com>
2022-07-19 08:57:28 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -136,7 +136,7 @@ if (iconMenu){
 }
 //=================================================================================================================================================
 // Класс для перевода url картинки в backgraund картинки
-function ibg() {
+window.onload = function ibg() {
         let ibg = document.querySelectorAll(".ibg");
         for (var i = 0; i < ibg.length; i++) {
         if(ibg[i].querySelector('img')) {
